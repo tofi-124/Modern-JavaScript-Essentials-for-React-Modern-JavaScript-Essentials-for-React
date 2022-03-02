7e9c0222dbf48cf15ec788af95620d3018325fd5
tofi-124 <tofikahm@yahoo.com>
2022-03-02 13:59:40 -0500

    Statements vs. Expressions

--- arrowfunction.js
+++ arrowfunction.js
@@ -50,3 +50,34 @@ console.log(person);
 const nums = [1, 2, 3];
 nums.push(4);
 console.log(nums);
+
+// Statements vs. Expressions
+
+// *** Statements
+
+/*
+let a = 1 + 2
+if(true) {
+  //
+}
+for(...)
+while(...)
+try {
+} catch(e) {
+}
+*/
+
+// *** Expressions
+var a = { hi: true };
+
+function stuff() {
+  return { hi: true };
+}
+
+if (Math.random() > 0.5) {
+  a = 'yes';
+} else {
+  a = 'no';
+}
+
+a = Math.random() > 0.5 ? 'yes' : 'no';
