bb8f8af282e3b96d4fe576fd9b5f70a1cf88a178
tofi-124 <tofikahm@yahoo.com>
2022-03-02 14:05:44 -0500

    Template Strings a.k.a. String Interpolation

--- arrowfunction.js
+++ arrowfunction.js
@@ -81,3 +81,19 @@ if (Math.random() > 0.5) {
 }
 
 a = Math.random() > 0.5 ? 'yes' : 'no';
+
+// Template Strings a.k.a. String Interpolation
+
+const firstName = 'Dave';
+const lastName = 'Ceddia';
+const usesReact = false;
+
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+const summary = `${fullName} ${usesReact ? 'uses React' : "doesn't use React"}`;
+console.log(summary);
+
+const add = (a, b) => a + b;
+const answer = `The answer is ${add(40, 2)}`;
+console.log(answer);
