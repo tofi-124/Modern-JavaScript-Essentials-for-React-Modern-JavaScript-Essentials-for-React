a2507a65a2b8e5821a7bc60fca9d7011593a1b3a
tofi-124 <tofikahm@yahoo.com>
2022-03-02 12:15:35 -0500

    using this in arrow function

--- arrowfunction.js
+++ arrowfunction.js
@@ -7,3 +7,10 @@ const FullNameUsingObj = (first, last, email) => ({
 console.log(FullNameUsingObj("tofik", "mohammed", "tofikahm@yahoo.com"));
 //Logs { first: 'tofik', email: 'tofikahm@yahoo.com', last: 'mohammed' }
 
+'use strict';
+
+const printThis = () => {
+  console.log(this);
+};
+
+printThis.bind(42)();
